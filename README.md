9860198a65b03943b75ac2825344a7373856face
mansip2002 <mansii.patel@mail.utoronto.ca>
2023-09-23 19:36:59 -0400

    Completed activity 3

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Activity 1: Flask and virtual env setup
 
 Activity 2: Hello activity
 ![Image](/images/helloMansi.png)
+
+Activity 3: Chapter 3 Example
+![Image](/images/activity3.png)
+![Image](/images/activity3additional.png)
--- hello.py
+++ hello.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
+
+moment = Moment(app)
+bootstrap = Bootstrap(app)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+   return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html',
+    current_time=datetime.utcnow())
